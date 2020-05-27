c07a573e9c911229353cbc9c69938f8c20d7941b
wangqingbin8 <wangqingbin8@jd.com>
2020-05-27 18:39:28 +0800

    增加base基类基本功能

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,9 +3,14 @@
 # File  : basepage.py
 # Author: wangqingbin8
 # Time  : 2020-05-25 16:00:57
+## desc: https://www.selenium.dev/documentation/zh-cn/getting_started/
+## desc: https://python-selenium-zh.readthedocs.io/zh_CN/latest/
 from loguru import logger
 from selenium import webdriver
+from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions, wait
+from selenium.webdriver.support.wait import WebDriverWait
 
 from utils.read_yaml import get_yaml_element
 
@@ -16,7 +21,7 @@ class BasePage:
         self.data = get_yaml_element('data/elements.yaml')
         self.driver = driver
 
-    def find_el(self, locator: dict):
+    def find_element(self, locator: dict):
         """
         By对象定位方式:
             ID = "id"
@@ -41,5 +46,78 @@ class BasePage:
         except Exception as e:
             logger.error(e)
 
+    def find_elements(self, locator: dict):
+        pass
+
     def get_value(self, element):
         return element.get_attribute('value')
+
+    def get_alert(self):
+        # alerts、confirms、prompts等对话框
+        # TODO alter对象 可以做确定、忽略、阅读提示文本或者甚至输入prompt
+        # alert = self.driver.switch_to_alert()
+
+        # 自动确认alter弹窗
+        # Wait for the alert to be displayed and store it in a variable
+        alert = wait.until(expected_conditions.alert_is_present())
+        # Store the alert text in a variable
+        text = alert.text
+        # Press the OK button
+        alert.accept()
+
+        # 自动确认Confirm确认框
+        # Wait for the alert to be displayed
+        wait.until(expected_conditions.alert_is_present())
+        # Store the alert in a variable for reuse
+        alert = driver.switch_to.alert
+        # Store the alert text in a variable
+        text = alert.text
+        # Press the Cancel button
+        alert.dismiss()
+
+        # 自动处理Prompt提示框
+        # Wait for the alert to be displayed
+        wait.until(expected_conditions.alert_is_present())
+        # Store the alert in a variable for reuse
+        alert = Alert(driver)
+        # Type your message
+        alert.send_keys("Selenium")
+        # Press the OK button
+        alert.accept()
+
+    def events(self):
+        """ 示例 """
+        # 浏览器向前
+        self.driver.forward()
+        # 浏览器向后
+        self.driver.back()
+        # And now output all the available cookies for the current URL
+        self.driver.get_cookies()
+        # 获取当前url
+        self.driver.current_url
+        # 获取当前url的网页标题
+        self.driver.title
+        # 刷新页面
+        self.driver.refresh()
+        # 获取当前窗口(返回窗口ID)
+        self.driver.current_window_handle
+        # 切换窗口或标签页  PS:该特性适用于 Selenium 4 及其后续版本
+        # self.driver.switch_to.new_window('window')  # 'tab'
+        # 获取窗口大小
+        # 或者存储尺寸并在以后查询它们
+        size = self.driver.get_window_size()
+        width = size.get("width")
+        height = size.get("height")
+        # 最大化窗口
+        self.driver.maximize_window()
+
+    def _waits(self):
+        # # 显示wait 使用expected_condition类处理各种情况
+        # wait = WebDriverWait(driver, 10)
+        # element = wait.until(EC.element_to_be_clickable((By.ID, 'someid')))
+
+        # # 隐式等待
+        # self.driver.implicitly_wait(10) # seconds
+
+        # 程序死等 time.sleep()
+        pass
--- pages/main_page.py
+++ pages/main_page.py
@@ -13,11 +13,20 @@ class MainPage(BasePage):
 
     def open_url(self, url):
         self.driver.get(url)
+        # 通过代理的方式打开url
+        #PROXY = "<HOST:PORT>"
+        # webdriver.DesiredCapabilities.CHROME['proxy'] = {
+        #    "httpProxy": PROXY,
+        #    "ftpProxy": PROXY,
+        #    "sslProxy": PROXY,
+        #    "proxyType": "MANUAL",
+        #}
+        #self.driver.get(url)
         return self.driver.current_url
 
     def search(self, keyword):
         locator = self.data.get('search')
-        element = self.find_el(locator)
+        element = self.find_element(locator)
         element.send_keys(keyword)
         text = self.get_value(element)
         return text
